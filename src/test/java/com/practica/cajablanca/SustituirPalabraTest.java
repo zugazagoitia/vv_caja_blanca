3d80aa2090574adbea6e5e71e1f994b0fd62250d
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2021-11-24 17:20:32 +0100

    Añadido test SustituirPalabraTest
    Signed-off-by: Alberto Zugazagoitia <alberto@zugazagoitia.com>

---
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -0,0 +1,38 @@
+package com.practica.cajablanca;
+
+import com.cajanegra.EmptyCollectionException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SustituirPalabraTest {
+
+
+	@Test
+	/**
+	 * Comprueba todas las aristas del grafo de flujo menos la 2->22
+	 */
+	void sustituirPalabraTest1() throws EmptyCollectionException {
+		Editor e = new Editor();
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		e.leerFichero(classLoader.getResource("fichero.txt").getPath());
+
+		e.sustituirPalabra("por", "en");
+		assertEquals("[Con, diez, cañones, en, banda]", e.getLinea(1).toString()); //Llamo a toString porque el equals está mal hecho
+		assertEquals("[en, su, bravura, el, Temido]", e.getLinea(7).toString());
+	}
+
+	@Test
+	/**
+	 * Comprueba únicamente la arista 2->22
+	 */
+	void sustituirPalabraTest2() {
+		Editor e = new Editor();
+
+		assertEquals(e.size(), 0);
+		e.sustituirPalabra("por", "en");
+		assertEquals(e.size(), 0);
+
+	}
+
+}
