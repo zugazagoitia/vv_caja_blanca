320cf343943ee9df9dae629462c2955cb15caac3
Gonzalo <gonsez@gmail.com>
2021-12-12 18:41:49 +0100

    Sustituir terminado y test de cada camino

--- src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -1,36 +1,77 @@
 package com.practica.cajablanca;
 
 import com.cajanegra.EmptyCollectionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SustituirPalabraTest {
 
+	Editor e;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		e = new Editor();
+	}
+
 	@Test
+	@DisplayName("Camino 1")
 	/**
 	 * Test Camino1 = 2, 21. Comprueba únicamente la arista 2->21
 	 */
 	void sustituirPalabraTestEditorSizeEmpty() {
-		Editor e = new Editor();
-
-		assertEquals(e.size(), 0);
+		e.leerFichero("src/test/resources/ficherovacio.txt");
 		e.sustituirPalabra("por", "en");
 		assertEquals(e.size(), 0);
 	}
 
 	@Test
+	@DisplayName("Camino 2")
 	/**
-	 * Comprueba todas las aristas del grafo de flujo menos la 2->22
+	 * Test Camino1 = 2, 3-4, 5-8, 9, 17-18, 19, 20, 21.
 	 */
-	void sustituirPalabraTest1() throws EmptyCollectionException {
-		Editor e = new Editor();
-		ClassLoader classLoader = this.getClass().getClassLoader();
-		e.leerFichero(classLoader.getResource("fichero.txt").getPath());
-
+	void sustituirPalabraTestListaSizeEmpty() {
+		e.leerFichero("src/test/resources/FicheroVacioConEspacio");
 		e.sustituirPalabra("por", "en");
-		assertEquals("[Con, diez, cañones, en, banda]", e.getLinea(1).toString()); //Llamo a toString porque el equals está mal hecho
-		assertEquals("[en, su, bravura, el, Temido]", e.getLinea(7).toString());
+		assertEquals(e.size(), 1);
 	}
+
+	@Test
+	@DisplayName("Camino 3")
+	/**
+	 * Test Camino1 = 2, 3-4, 5-8, 9, 17-18, 19, 5-8, 9, 17-18, 19, 20, 22
+	 */
+	void sustituirPalabraTestListaDosVecesSizeEmpty() {
+		e.leerFichero("src/test/resources/FicheroVacioCon2Espacios");
+		e.sustituirPalabra("por", "en");
+		assertEquals(e.size(), 2);
+	}
+
+	@Test
+	@DisplayName("Camino 4")
+	/**
+	 * Test Camino1 = 2, 3-4, 5-8, 9, 10, 13, 15, 9, 17-18, 19, 20, 22
+	 */
+	void sustituirPalabraTestNoSeEncuentraPalabra() throws EmptyCollectionException {
+		e.leerFichero("src/test/resources/Coche");
+		e.sustituirPalabra("por", "en");
+		assertEquals(e.size(), 1);
+		assertEquals("[Coche]", e.getLinea(1).toString());
+	}
+
+	@Test
+	@DisplayName("Camino 5")
+	/**
+	 * Test Camino1 = 2, 3-4, 5-8, 9, 10, 13, 15, 9, 17-18, 19, 20, 22
+	 */
+	void sustituirPalabraTestSeEncuentraPalabra() throws EmptyCollectionException {
+		e.leerFichero("src/test/resources/por");
+		e.sustituirPalabra("por", "en");
+		assertEquals(e.size(), 1);
+		assertEquals("[en]", e.getLinea(1).toString());
+	}
+
 
 }
